5f98c662634ae86bf7a0f72cc7f698629668444b
iamkaikai <iamkaikai@livemail.tw>
2023-09-12 08:52:57 -0400

    init commit

---
+++ chat.js
@@ -0,0 +1,22 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+client.on('message', message => {
+    console.log(message.body);
+	if(message.body === 'ping') {
+        console.log('receive ping')
+		message.reply('pong');
+	}
+});
